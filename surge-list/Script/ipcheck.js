e05b63b03bda5aa7758e7aceb8f280e597a0f502
TOM <89062432+TOM69JK@users.noreply.github.com>
2023-04-01 00:14:38 +0800

    Update ipcheck.js

--- surge-list/Script/ipcheck.js
+++ surge-list/Script/ipcheck.js
@@ -35,7 +35,7 @@ if ($trigger == "button") {
 };
 $done({
     title:"👑𝕾𝖀𝕽𝕲𝕰👑ᴾᴿᴼ",
-    content:"🌗➟"+(new Date()).Format("yyyy-MM-dd 🪐➟HH:mm:ss")+"\n🛰星链运行🛰➟"+startTime + "\n🚥➟Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
+    content:"🌞➟"+(new Date()).Format("yyyy-MM-dd 🌜➟HH:mm:ss")+"\n🛰星链运行🛰➟"+startTime + "\n🚥➟Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"crown.fill":"crown",
    "icon-color":icon_s?"#FFFF00":"#000000"
 });
